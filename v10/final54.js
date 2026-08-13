6dd10400ff0b179e15270273e7763879dae31614
fahadali1991 <faq1411@gmail.com>
2026-08-14 01:08:47 +0300

    Add V54 final preview

---
+++ v10/final54.js
@@ -0,0 +1,2 @@
+import {finalDocument as base} from './final-ui-v5.js';
+export function finalDocument(s){const html=base(s);return html+`<div class="toolbar"><button class="btn primary" data-action="new-document">إنشاء تقرير جديد</button></div>`}
