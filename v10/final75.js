2b12254598b48b745ab1907ea55f24ff8c13d3cf
fahadali1991 <faq1411@gmail.com>
2026-08-14 08:33:19 +0300

    V75 final wrapper

---
+++ v10/final75.js
@@ -0,0 +1,2 @@
+import {finalDocument75 as base} from './final-ui75.js?v=75';
+export function finalDocument75(s){return base(s)+`<div class="row" style="position:static;margin:0 0 48px"><button class="btn primary" data-action="new-document">إنشاء تقرير جديد</button></div>`}
