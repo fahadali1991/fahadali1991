4507f91525155d2d86c9172df8e3297f59d6f999
fahadali1991 <faq1411@gmail.com>
2026-08-14 03:34:50 +0300

    Adjust goals step for family details flow

---
+++ v10/goals57.js
@@ -0,0 +1,2 @@
+import {goalsPage as base} from './renderers-v5.js';
+export function goalsPage57(state){return base(state).replace('data-action="understanding"','data-action="back-family-details"').replace('الخطوة الثانية','الخطوة الثالثة').replace('التالي: اختيار الوصف','التالي: العنوان')}
