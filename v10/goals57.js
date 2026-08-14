070310a7d1666f00c7b036cc0098db4ed125ee3b
fahadali1991 <faq1411@gmail.com>
2026-08-14 05:06:46 +0300

    Route goals page to semantic goal engine

--- v10/goals57.js
+++ v10/goals57.js
@@ -1,2 +1,1 @@
-import {goalsPage as base} from './renderers-v5.js';
-export function goalsPage57(state){return base(state).replace('data-action="understanding"','data-action="back-family-details"').replace('الخطوة الثانية','الخطوة الثالثة').replace('التالي: اختيار الوصف','التالي: العنوان')}
+export {goalsPage64 as goalsPage57} from './goals64.js?v=64';
