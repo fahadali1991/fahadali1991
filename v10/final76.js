10a2b073ea9f0ff64a513f85e7e9d94b09ea4754
fahadali1991 <faq1411@gmail.com>
2026-08-14 09:05:38 +0300

    V76: final wrapper

---
+++ v10/final76.js
@@ -0,0 +1,2 @@
+import {finalDocument76 as base} from './final-ui76.js?v=76';
+export function finalDocument76(s){return base(s)+`<div class="row" style="position:static;margin:0 0 48px"><button class="btn primary" data-action="new-document">إنشاء تقرير جديد</button></div>`}
