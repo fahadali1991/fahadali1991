9c8f981867f0c6ddf7b860c24fb457286bec281e
fahadali1991 <faq1411@gmail.com>
2026-08-14 21:07:09 +0300

    V85: apply contextual extraction on top of V84 understanding core

---
+++ v10/engine85.js
@@ -0,0 +1,4 @@
+import * as core from './engine84.js?v=85';
+export * from './engine84.js?v=85';
+import {applyContext85} from './context85.js?v=85';
+export function analyze(raw,entryIntent='smart'){return applyContext85(core.analyze(raw,entryIntent))}
