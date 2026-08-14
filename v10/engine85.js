598292d9f9e7ff9050b6a9f51786062b41c2fe45
fahadali1991 <faq1411@gmail.com>
2026-08-14 21:11:31 +0300

    V85: use refined contextual extractor

--- v10/engine85.js
+++ v10/engine85.js
@@ -1,4 +1,4 @@
 import * as core from './engine84.js?v=85';
 export * from './engine84.js?v=85';
-import {applyContext85} from './context85.js?v=85';
+import {applyContext85} from './context85b.js?v=85b';
 export function analyze(raw,entryIntent='smart'){return applyContext85(core.analyze(raw,entryIntent))}
