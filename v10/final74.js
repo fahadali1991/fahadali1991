5ba490abe2eb876b5f83fecd6fb89096572f588b
fahadali1991 <faq1411@gmail.com>
2026-08-14 08:24:23 +0300

    V74 final wrapper

---
+++ v10/final74.js
@@ -0,0 +1,2 @@
+import {finalDocument74 as base} from './final-ui74.js?v=74';
+export function finalDocument74(s){return base(s)+`<div class="row" style="position:static;margin:0 0 48px"><button class="btn primary" data-action="new-document">إنشاء تقرير جديد</button></div>`}
